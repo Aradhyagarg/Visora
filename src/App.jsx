f4e15b05c1e3d99b46298e5a8ca69f1905f07180
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2025-09-13 18:16:46 +0530

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import BlogTitles from './pages/BlogTitles'
+import Community from './pages/Community'
+import Dashboard from './pages/Dashboard'
+import GenerateImages from './pages/GenerateImages'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import WriteArticle from './pages/WriteArticle'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<Dashboard />}/>
+          <Route path='write-article' element={<WriteArticle/>}/>
+          <Route path='blog-titles' element={<BlogTitles/>}/>
+          <Route path='generate-images' element={<GenerateImages/>}/>
+          <Route path='remove-background' element={<RemoveBackground/>}/> 
+          <Route path='remove-object' element={<RemoveObject/>}/> 
+          <Route path='review-resume' element={<ReviewResume/>}/>
+          <Route path='community' element={<Community/>}/>
+        </Route>
+      </Routes>
     </>
   )
 }
